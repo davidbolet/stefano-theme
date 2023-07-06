d23e38a0267d9769bd6ca6dcee9151da237c81e6
David Bolet <davidbolet@gmail.com>
2023-07-06 12:38:35 +0200

    added elementor support

--- stefano-theme/functions.php
+++ stefano-theme/functions.php
@@ -5,7 +5,10 @@ function my_theme_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_styles' );
 
-
+function mytheme_add_elementor_support() {
+    add_theme_support( 'elementor' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_elementor_support' );
 
 function your_theme_custom_logo_setup() {
     $defaults = array(
